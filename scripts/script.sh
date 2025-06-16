dd827db0555ea0d6e7a0f3ee160cc86ea624c76d
romit-gandhi <romit.gandhi@rapidops.com>
2025-06-16 11:54:50 +0530

    Fix

---
+++ scripts/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the argument passed from the workflow
+ARGUMENT=$1
+
+# Example: Print the argument (or process it as needed)
+echo "Received argument: $ARGUMENT"
+
+# Add your script logic here
