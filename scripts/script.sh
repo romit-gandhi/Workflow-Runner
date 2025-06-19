64e4777ec873bddd9461a48e654b6137c0e8aef1
romit-gandhi <romit.gandhi@rapidops.com>
2025-06-19 19:24:51 +0530

    Fix

--- scripts/script.sh
+++ scripts/script.sh
@@ -9,17 +9,6 @@ echo "Argument: $1"
 # Get the argument (secret)
 SECRET="$1"
 
-# Get the length of the secret
-LENGTH=${#SECRET}
-echo "Argument length: $LENGTH"
-
-echo "BELOW IS THE SECRET"
-
-echo "All characters with positions:"
-for ((i=0; i<LENGTH; i++)); do
-  CHAR="${SECRET:$i:1}"
-  echo "Character at position $((i+1)): $CHAR"
-done
 
 # Run terraform version command
 echo "Running terraform version..."
